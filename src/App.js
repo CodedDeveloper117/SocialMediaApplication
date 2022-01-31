87b4994196626994e5963b81ec5227932ff5d942
CodeDev <58920970+CodeDev-pro@users.noreply.github.com>
2022-01-31 16:50:50 +0100

    Implemented Not Found Page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom'
 import Login from './components/Login'
 import Home from './container/Home';
+import NotFound from './components/NotFound'
 
 const App = () => {
   const navigate = useNavigate();
   const [loggedOut, setLoggedout] = useState(false)
   return (
       <Routes>
+        <Route path="*" element={<NotFound />} />
         <Route path="login" element={<Login loggedOut={loggedOut} setLoggedOut={setLoggedout} />} />
         <Route path="/*" element={<Home setLoggedOut={setLoggedout} />} />
       </Routes>
